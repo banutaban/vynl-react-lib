a432c76551acae88cd322fcbf441c2a8a0e1ca7a
Öner Zafer <onerizafer@gmail.com>
2019-04-20 15:49:06 +0200

    little bit grid and responsiveness

--- src/components/VynlContainer/VynlContainer.js
+++ src/components/VynlContainer/VynlContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../variables.css';
 import './VynlContainer.css';
 
 export const VynlContainer = ({ children }) => <div className='vynl-container'>{children}</div>;
